b18b881668127beef721bbd1c18cb46a03164067
Guan612 <tiantony53@163.com>
2025-12-01 19:23:40 +0800

    拆分逻辑

---
+++ src/api/index.ts
@@ -0,0 +1,30 @@
+import { invoke } from "@tauri-apps/api/core";
+import { DashboardStats, MasterWord, ReviewCard } from "../types";
+
+export const dueWordsAPI = () => {
+  return invoke<ReviewCard[]>("get_due_words");
+};
+
+export const masterWordsAPI = () => {
+    return invoke<MasterWord[]>("get_all_master_words");
+};
+
+export const dashboardStatsAPI = () => {
+  return invoke<DashboardStats>("get_dashboard_stats");
+};
+
+// 注意：Tauri 会自动把 JS 的驼峰命名 (masterId)
+// 映射到 Rust 的下划线命名 (master_id)
+export const addToLearningAPI = (masterId: number) => {
+  return invoke("add_word_to_learning", { masterId });
+};
+
+// 关键：键名必须和 Rust 函数参数名对应 (驼峰转下划线)
+// Rust: user_word_id -> JS: userWordId
+// Rust: rating_val   -> JS: ratingVal
+export const submitReviewAPI = (ratingVal: number, userWordId: number) => {
+  return invoke("submit_review", {
+    userWordId: userWordId,
+    ratingVal: ratingVal,
+  });
+};
--- src/stores/wordStore.ts
+++ src/stores/wordStore.ts
@@ -1,10 +1,18 @@
 import { invoke } from "@tauri-apps/api/core";
-import { MasterWord, ReviewCard } from "../types";
+import { DashboardStats, MasterWord, ReviewCard } from "../types";
 import { create } from "zustand";
+import {
+  addToLearningAPI,
+  dashboardStatsAPI,
+  dueWordsAPI,
+  masterWordsAPI,
+  submitReviewAPI,
+} from "../api";
 
 interface WordState {
   masterWords: MasterWord[];
   reviewQueue: ReviewCard[];
+  stats: DashboardStats | null;
   isLoading: boolean;
   error: string | null;
 
@@ -15,18 +23,20 @@ interface WordState {
   //拉取学习任务
   fetchDueWords: () => Promise<void>;
   submitReview: (userWordId: number, ratingVal: number) => Promise<void>;
+  fetchStats: () => void;
 }
 
 export const useWordStore = create<WordState>((set) => ({
   masterWords: [],
   reviewQueue: [],
+  stats: null,
   isLoading: false,
   error: null,
 
   fetchMasterWords: async () => {
     set({ isLoading: true, error: null });
     try {
-      const words = await invoke<MasterWord[]>("get_all_master_words");
+      const words = await masterWordsAPI();
       console.log("Fetched words:", words); // 调试用
       set({ masterWords: words, isLoading: false });
     } catch (err) {
@@ -38,9 +48,7 @@ export const useWordStore = create<WordState>((set) => ({
 
   addToLearning: async (masterId: number) => {
     try {
-      // 注意：Tauri 会自动把 JS 的驼峰命名 (masterId)
-      // 映射到 Rust 的下划线命名 (master_id)
-      await invoke("add_word_to_learning", { masterId });
+      await addToLearningAPI(masterId);
 
       console.log(`Successfully added word ${masterId} to learning!`);
       // 这里未来可以加一个 Toast 提示或者更新本地状态，暂时先打印日志
@@ -54,9 +62,7 @@ export const useWordStore = create<WordState>((set) => ({
     set({ isLoading: true, error: null });
     try {
       // 调用我们在 commands.rs 里写的 get_due_words
-      const cards = await invoke<ReviewCard[]>("get_due_words");
-
-      console.log("Due words fetched:", cards);
+      const cards = await dueWordsAPI();
       set({ reviewQueue: cards, isLoading: false });
     } catch (err) {
       console.error("Failed to fetch due words:", err);
@@ -67,22 +73,37 @@ export const useWordStore = create<WordState>((set) => ({
   submitReview: async (ratingVal: number, userWordId: number) => {
     try {
       // 1. 调用后端
-      // 关键：键名必须和 Rust 函数参数名对应 (驼峰转下划线)
-      // Rust: user_word_id -> JS: userWordId
-      // Rust: rating_val   -> JS: ratingVal
-      await invoke("submit_review", {
-        userWordId: userWordId,
-        ratingVal: ratingVal,
-      });
+      await submitReviewAPI(ratingVal, userWordId);
 
       // 2. 【关键优化】本地乐观更新
       // 提交成功后，立刻从复习队列中移除当前这张卡片，UI 会自动显示下一张
-      set((state) => ({
-        reviewQueue: state.reviewQueue.filter((card) => card.id !== userWordId),
-      }));
+      set((state) => {
+        // 先把这张卡片找出来
+        const currentCard = state.reviewQueue.find((c) => c.id === userWordId);
+        // 把除了这张卡片以外的剩下的排好
+        const remaining = state.reviewQueue.filter((c) => c.id !== userWordId);
+
+        if (ratingVal == 1 && currentCard) {
+          return { reviewQueue: [...remaining, currentCard] };
+        } else {
+          return { reviewQueue: remaining };
+        }
+      });
     } catch (err) {
       console.error("Failed to submit review:", err);
       // 这里最好不要清空队列，只是打印错误，或者弹个 Toast
+    }
+  },
+
+  fetchStats: async () => {
+    try {
+      // 1. 调用 invoke，并告诉它返回的是 DashboardStats 类型
+      // 2. 用 const data 接住返回值！
+      const data = await dashboardStatsAPI();
+      // 3. 将拿到的数据存入 Store
+      set({ stats: data });
+    } catch (err) {
+      console.error("Failed to fetch stats:", err);
     }
   },
 }));
